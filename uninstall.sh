99618f32d743dd75823548b07a5c09059c277e62
ipdev99 <ipdev99@gmail.com>
2023-10-01 18:26:52 -0500

    Update

--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,15 @@
 ## This script is part of the iYT module project.
 ## ipdev @ xda-developers
 
+## Module directory(s) set by Magisk.
+MODDIR=${0%/*}
+
+# Module Directory
+MDIR=${0%/*}
+# Magisk Modules Directory
+MMDIR=${MDIR%/*}
+# ADB Directory
+ADB=${MMDIR%/*}
+
 # Remove the additional iYT mount directory.
-rm -rf /data/adb/iyt
+rm -rf "$ADB"/iyt
